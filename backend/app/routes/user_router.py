2fc27600642aae2f83d4d27532dcd8a91234f5dc
Blas Antunez <blasantunez310@gmail.com>
2026-03-19 20:01:58 -0400

    [backend] post and patch to create and update particular fields of a user is working. user_router.py

--- backend/app/routes/user_router.py
+++ backend/app/routes/user_router.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlmodel import Session, select
 
 from app.db import get_session
-from app.models.user import User, UserCreate, UserPublic
+from app.models.user import User, UserCreate, UserPublic, UserUpdate
 from app.core.security import hash_password
 
 user_router = APIRouter(prefix="/users", tags=["users"])
@@ -39,3 +39,28 @@ def create_user(user_data: UserCreate, session: Session = Depends(get_session)):
     session.refresh(new_user)
 
     return new_user
+
+@user_router.patch('/{user_id}', response_model=UserPublic)
+def update_user(
+    user_id: int,
+    user_data: UserUpdate,
+    session: Session = Depends(get_session),
+):
+    user = session.get(User, user_id)
+
+    if not user:
+        raise HTTPException(status_code=404, detail='User not found')
+    
+    user_data_dump = user_data.model_dump(exclude_unset=True)
+
+    if 'password' in user_data_dump:
+        user.hashed_password = hash(user_data_dump.pop('password'))
+
+    for key, value in user_data_dump.items():
+        setattr(user, key, value)
+
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    return user
