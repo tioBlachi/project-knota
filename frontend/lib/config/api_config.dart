078b368667e9dfa3da618b5b6105a2ad1263aa2d
Blas Antunez <blasantunez310@gmail.com>
2026-03-31 23:58:32 -0400

    [feat] implemented alembic and have v1 of the app successfully running the backend on lambda with the database on neon

--- frontend/lib/config/api_config.dart
+++ frontend/lib/config/api_config.dart
@@ -1,7 +1,7 @@
 class ApiConfig {
   static const String _configuredBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://192.168.4.117:80',
+    defaultValue: 'https://3onb2vpyn3k4ramtqd3yvqk3py0uecfj.lambda-url.us-east-1.on.aws/',
   );
 
   static String get baseUrl => _configuredBaseUrl;
